7367a4c5bd291eb88814f54cd932694e303ee746
Leandro Vieira <leandradevieira38@gmail.com>
2024-05-30 13:53:24 -0300

    Modifying routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Produto = require("../models/Products");
 
 // Create a new product
-router.post("create/", async (req, res) => {
+router.post("/create/", async (req, res) => {
   const { name, description, category, brand, model, price, stock, active } =
     req.body;
 
@@ -33,7 +33,7 @@ router.post("create/", async (req, res) => {
 });
 
 // Get all products
-router.get("getAll/", async (req, res) => {
+router.get("/getAll/", async (req, res) => {
   try {
     const produtos = await Produto.find();
     res.status(200).json(produtos);
@@ -46,7 +46,7 @@ router.get("getAll/", async (req, res) => {
 });
 
 // Get one product by ID
-router.get("getById/:id", async (req, res) => {
+router.get("/getById/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const produto = await Produto.findById(id);
@@ -63,7 +63,7 @@ router.get("getById/:id", async (req, res) => {
 });
 
 // Update a product by ID (PUT)
-router.put("update/:id", async (req, res) => {
+router.put("/update/:id", async (req, res) => {
   const { id } = req.params;
   const { name, description, category, brand, model, price, stock, active } =
     req.body;
@@ -104,7 +104,7 @@ router.put("update/:id", async (req, res) => {
 });
 
 // Delete a product by ID
-router.delete("delete/:id", async (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
